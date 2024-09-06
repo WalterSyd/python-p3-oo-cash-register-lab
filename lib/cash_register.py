4644a9acc9588a0865185d33c74abeb2572c8f91
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-06 12:57:04 +0300

    Added discount functionality

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -7,6 +7,14 @@ class CashRegister:
     self.discount = discount
     self.items = []
 
+  #adds items, accepts optional quantity to calculate totals with multiple items
   def add_item(self, title, price, quantity = 1):
     self.items.append((title, price, quantity))
     self.total += price * quantity
+
+  def apply_discount(self):
+    if self.discount > 0:
+      self.total -= (self.total *(self.discount / 100))
+      print(f"After the discount, the total comes to ${self.total:.0f}.")
+    else:
+      print("There is no discount to apply.")
