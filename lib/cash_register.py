4a5eed0ebba3d494ba43eda9f9ed8cb53047b752
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-06 12:24:50 +0300

    tests for initialization and add items passed

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+
+  def __init__(self, discount=0):
+    self.total = 0
+    self.discount = discount
+    self.items = []
+
+  def add_item(self, title, price, quantity = 1):
+    self.items.append((title, price, quantity))
+    self.total += price * quantity
