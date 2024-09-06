00a090690d5af1272821d2a82625c4666812fdfc
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-06 13:54:32 +0300

    completed test cases

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -6,15 +6,28 @@ class CashRegister:
     self.total = 0
     self.discount = discount
     self.items = []
+    self.last_transaction = (0,0)
 
   #adds items, accepts optional quantity to calculate totals with multiple items
   def add_item(self, title, price, quantity = 1):
-    self.items.append((title, price, quantity))
+    self.items.extend([title] *  quantity)
     self.total += price * quantity
+    self.last_transaction = (price, quantity, title)# Update last transaction details
 
   def apply_discount(self):
     if self.discount > 0:
-      self.total -= (self.total *(self.discount / 100))
+      discount_amount = self.total * (self.discount / 100)
+      self.total -= discount_amount #subtract discount from self.total 
       print(f"After the discount, the total comes to ${self.total:.0f}.")
     else:
       print("There is no discount to apply.")
+
+  def void_last_transaction(self):
+    price, quantity, title = self.last_transaction # Get last transaction details by unpacking tuple
+    if price > 0:   #Checks if last ransaction is valid
+      self.total -= price * quantity  #Subtracts last transaction price from total price
+      for _ in range(quantity):
+        self.items.remove(title)  #removes the item from items list for each quantity
+      self.last_transaction = (0,0,"")    #resets the last transaction details to ("") if transaction voided
+    else:
+      print("There is no transaction to void")
